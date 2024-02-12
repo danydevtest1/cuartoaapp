0ccadd3c1ea19986b725d88a03d66945c2c226b9
Dany Cambrano <danydevtest@gmail.com>
2024-02-12 11:32:57 -0600

    Se agrego una nueva carpeta con un archivo de datos

--- src/components/Lista.js
+++ src/components/Lista.js
@@ -1,30 +1,10 @@
 import React, {useState} from 'react';
 import Datos from './Datos';
+import {lista} from "../datos/ListDatos";
 
 export default function Lista() {
-    const [datos, setDatos]=useState([
-        {
-            "id":"01",
-            "nombre":"Dany Cambrano Arcos",
-            "tel":"9341052345",
-            "direccion":"Balancan",
-            "imag":"https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Cuesta_del_obispo_01.jpg/800px-Cuesta_del_obispo_01.jpg"
-        },
-        {
-            "id":"02",
-            "nombre":"Francisco Lopez",
-            "tel":"9341052345",
-            "direccion":"Zapata",
-            "imag":"https://concepto.de/wp-content/uploads/2015/03/paisaje-e1549600034372.jpg"
-        },
-        {
-            "id":"03",
-            "nombre":"Jose Antonio",
-            "tel":"9341052345",
-            "direccion":"Tenosique",
-            "imag":"https://img.freepik.com/foto-gratis/paisaje-forestal_71767-127.jpg"
-        }
-    ])
+   
+    const [datos, setDatos]=useState(lista)
   return (
     <div>
         {
---
+++ src/datos/ListDatos.js
@@ -0,0 +1,23 @@
+export const lista=[
+    {
+        "id":"01",
+        "nombre":"Dany Cambrano Arcos",
+        "tel":"9341052345",
+        "direccion":"Balancan",
+        "imag":"https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Cuesta_del_obispo_01.jpg/800px-Cuesta_del_obispo_01.jpg"
+    },
+    {
+        "id":"02",
+        "nombre":"Francisco Lopez",
+        "tel":"9341052345",
+        "direccion":"Zapata",
+        "imag":"https://concepto.de/wp-content/uploads/2015/03/paisaje-e1549600034372.jpg"
+    },
+    {
+        "id":"03",
+        "nombre":"Jose Antonio",
+        "tel":"9341052345",
+        "direccion":"Tenosique",
+        "imag":"https://img.freepik.com/foto-gratis/paisaje-forestal_71767-127.jpg"
+    }
+]
